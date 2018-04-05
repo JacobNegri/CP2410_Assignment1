71b3bb528d3c53b7d9791f9744e78caba10cce93
Jacob Negri <jc319816@ad.jcu.edu.au>
2018-04-05 21:49:31 +1000

    Initial commit

---
+++ SparseArray.py
@@ -0,0 +1,28 @@
+# Assignment 1
+# . Sparse Array with a Linked list implementation
+#
+# Name: Jacob Negri
+# SN: 13165429
+
+class SparseArray(object):
+
+    def _init_(n):
+
+
+
+    def fill(seq):
+    
+
+    def __getitem__(self, j):
+
+
+
+    def __setitem__(self, key, value):
+
+
+    def __len__(self):
+
+
+    def get_usage(self):
+
+
