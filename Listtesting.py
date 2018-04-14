d5f2043344717548b2f55fe95d010381e1348471
jacob <jnegr3@gmail.com>
2018-04-14 20:45:23 +1000

    ....

---
+++ Listtesting.py
@@ -0,0 +1,35 @@
+from time import clock
+import random
+
+
+for q in range(3,9):
+    print("")
+    n = 10**q
+    print("n: " + n)
+    array = [None]*n
+    Ms = []
+    for f in range(0,q):
+        if f < 8:
+            Ms.append(10 ** f)
+
+    for m in Ms:
+        print("m: " + m)
+        for o in range(1, m):
+            numer = random.randint(0, n - 1)
+            array[numer] = 3
+        num = random.randint(0, n - 1)
+        start = clock()
+        array[num] = 3
+        end = clock()
+        print("Set: {0:.5f} ms".format((end - start)*1000))
+        num = random.randint(0, n - 1)
+        start = clock()
+        array[num]
+        end = clock()
+        print("Get: {0:.5f} ms".format((end - start)*1000))
+
+
+
+
+
+
